cd2e2ee535c39f81978c6e54623ca61d221ebf8b
damndest <groundbreaking@nate.com>
2024-05-28 11:07:00 +0900

    메인 공통 화면 작업

--- job/settings.py
+++ job/settings.py
@@ -57,8 +57,6 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'main/template'),
-            os.path.join(BASE_DIR, 'foods/template'),
             os.path.join(BASE_DIR, 'template'),
         ],
         'APP_DIRS': True,
@@ -122,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    ("img", os.path.join(BASE_DIR, 'img')),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
