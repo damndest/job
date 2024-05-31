eaf49e27c39957c610b5db6d36446a0e9fa8f279
damndest <groundbreaking@nate.com>
2024-05-31 11:47:00 +0900

    admin 사이트 등록 추가

--- humor/admin.py
+++ humor/admin.py
@@ -1,11 +1,3 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from main.models import Member
 
 # Register your models here.
-class MemberAdmin(UserAdmin):
-    model = Member();
-
-    list_display = ['member_idx', 'user_id', 'email', 'phone_num'];
-
-admin.site.register(Member, MemberAdmin);
