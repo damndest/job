71cacbba8a5419647b494798b90f58e63a7fc970
damndest <groundbreaking@nate.com>
2024-05-31 11:47:57 +0900

    음식 지역별 목록 껍데기 추가

--- foods/models.py
+++ foods/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from main.models import Member
 
 # Create your models here.
+class Food(models.Model):
+    food_no = models.AutoField(primary_key=True);
+    writer = models.CharField(max_length=50, blank=False, null=False);
+    title = models.CharField(max_length=255, blank=False, null=False);
+    content = models.TextField(null=False);
+    food_type = models.CharField(max_length=100);
+    area_name = models.CharField(max_length=255);
+    regist_date = models.DateTimeField(null=False);
+    modify_date = models.DateTimeField(null=False);
+    see_cnt = models.IntegerField(default=0);
+    member_idx = models.ForeignKey(
+        Member, on_delete=models.CASCADE
+    );
+
+    def __str__(self):
+        return self.title;
