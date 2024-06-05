22ea0e81b2e058faffb43423b44f81804e7df9d6
damndest <groundbreaking@nate.com>
2024-06-05 11:42:59 +0900

    음식 지역별 파일 추가 및 제거 로직 추가

--- foods/urls.py
+++ foods/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('area/<int:food_no>/', views.foods_area_detail, name='area_detail'),
     path('area/<int:food_no>/mod/', views.mod_foods_area, name='mod_area'),
     path('area/<int:food_no>/del/', views.del_foods_area, name='del_area'),
+    path('area/download/<int:food_no>/<str:file_name>', views.download_foods_area, name='download_area'),
+    path('area/del_file/<int:food_no>/<str:file_name>/', views.del_file_foods_area),
     path('area/del_chk_list/', views.del_chk_foods_area, name='del_chk_area'),
     path('area/reply/add/', views.add_rpl_foods_area, name='add_rpl_area'),
     path('area/reply/del/', views.del_rpl_foods_area, name='del_rpl_area'),
---
+++ static/js/food/area/mod.js
@@ -0,0 +1,3 @@
+function delFoodAreaFile(foodNo, fileNm) {
+    location.href = '/foods/area/del_file/' + foodNo + '/' + fileNm + '/';
+}
