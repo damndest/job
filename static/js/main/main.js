3a16e130dc95284c7bff5c76c0d7a900e3e4cc02
hcm1206 <hcm1206@naver.com>
2024-06-07 13:23:48 +0900

    메인(main, template) 페이지에 앱 링크 적용(임시)

---
+++ static/js/main/main.js
@@ -0,0 +1,15 @@
+const foods = () => {
+    location.href = "/foods/area/list/1/";
+}
+
+const humor = () => {
+    location.href = "/humor/domestic/";
+}
+
+const sports = () => {
+    location.href = "/sports/";
+}
+
+const economy = () => {
+    location.href = "/economy/financemain/";
+}
